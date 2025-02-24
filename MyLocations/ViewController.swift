ca98e90aa9b32c15e475e064810b8ea0301a5b9d
Gaspare Monte <gaspare.monte@rawfish.com>
2025-02-24 12:15:21 +0100

    Initial Commit

---
+++ MyLocations/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyLocations
+//
+//  Created by Gaspare Monte on 24/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
